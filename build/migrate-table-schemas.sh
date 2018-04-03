785421385e18f90ea959b64d4ef1f7160f1e18f8
Stefano Da Ros <sdaros@cip.li>
2018-04-03 15:59:02 +0200

    Update table data and descriptor

--- build/migrate-table-schemas.sh
+++ build/migrate-table-schemas.sh
@@ -9,6 +9,11 @@ section_code char(1) not null,
 course_code char(4) not null,
 organization_code varchar(8) not null);
 
+CREATE TABLE IF NOT EXISTS class_teacher (
+id integer not null primary key,
+class_id integer not null,
+teacher_id integer not null);
+
 CREATE TABLE IF NOT EXISTS teacher (
 id integer not null primary key,
 code char(6) not null);
