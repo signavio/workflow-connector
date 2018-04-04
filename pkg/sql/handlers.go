30cf6bd6faf3deca5cb36a85cf4f1bcb9624f11b
Stefano Da Ros <sdaros@cip.li>
2018-04-04 17:55:56 +0200

    Catch No LastInsertID error and proceed

--- pkg/sql/handlers.go
+++ pkg/sql/handlers.go
@@ -3,11 +3,14 @@ package sql
 import (
 	"database/sql"
 	"fmt"
+	"errors"
 
 	"github.com/signavio/workflow-connector/pkg/config"
 	"github.com/signavio/workflow-connector/pkg/log"
 	"github.com/signavio/workflow-connector/pkg/util"
 )
+
+var ErrNoLastInsertID = errors.New("Database does not support getting the last inserted ID")
 
 func (r *getSingle) handle() (results []interface{}, err error) {
 	log.When(r.backend.Cfg).Infoln("[handler] getSingle")
@@ -213,10 +216,18 @@ func (r *createSingle) handle() (results []interface{}, err error) {
 	if err != nil {
 		return nil, err
 	}
-	log.When(r.backend.Cfg).Infoln("[handler <-> handlers] return the" +
+	log.When(r.backend.Cfg).Infoln("[handler <-> handlers] return the " +
 		"newly created resource:\n call getSingle.handle()",
 	)
-	return r.getJustCreated(result)
+	results, err = r.getJustCreated(result)
+	if err == ErrNoLastInsertID {
+		// Return only a http.StatusCode 200
+		return []interface{}{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
 }
 func (r *createSingle) getJustCreated(result sql.Result) (results []interface{}, err error) {
 	// TODO: Figure out how to handle result.RowsAffected()
