0a450b7bd4c526d9847a81154c4fd1f23eb794b5
Stefano Da Ros <sdaros@cip.li>
2018-04-04 13:10:33 +0200

    Use proper foreign key references in schema migration script

--- build/migrate-table-schemas.sh
+++ build/migrate-table-schemas.sh
@@ -5,30 +5,30 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS class (
 id integer not null primary key,
-section_code char(1) not null,
-course_code char(4) not null,
-organization_code varchar(8) not null);
+section_code char(1) references section(code),
+course_code char(4) references course(code),
+organization_code varchar(8) references organization(code);
 
 CREATE TABLE IF NOT EXISTS class_teacher (
 id integer not null primary key,
-class_id integer not null,
-teacher_id integer not null);
+class_id integer references class(id),
+teacher_id integer references teacher(code);
 
 CREATE TABLE IF NOT EXISTS teacher (
 id integer not null primary key,
-code char(6) not null);
+code char(6) unique not null);
 
 CREATE TABLE IF NOT EXISTS organization (
 id integer not null primary key,
-code varchar(8) not null);
+code varchar(8) unique not null);
 
 CREATE TABLE IF NOT EXISTS section (
 id integer not null primary key,
-code char(1) not null);
+code char(1) unique not null);
 
 CREATE TABLE IF NOT EXISTS course (
 id integer not null primary key,
-code char(4) not null);
+code char(4) unique not null);
 
 COMMIT;
 __EOF__
